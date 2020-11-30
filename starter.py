c33e8487c5901a4e8748c224b324fc60f18a89de
takticale <59446045+takticale@users.noreply.github.com>
2020-11-29 19:57:22 -0500

    Update starter.py

--- starter.py
+++ starter.py
@@ -6,9 +6,10 @@ def add(num1, num2):
  
 # CODE
 
-operation = input("Enter an operation\n=> add\n=>subtract\n=>multiply\n=>divide\n")
+operation = input("Enter an operation\n=> add\n=> subtract\n=> multiply\n=> divide\n")
 first = int(input("Enter a number: "))
 second = int(input("Enter another number: "))
 
 if operation == "add":
+  print("The sum of the two numbers is")
   add(first, second)
