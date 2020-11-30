f357ee71049ed3f59f13c70584f6e906627fc6af
takticale <59446045+takticale@users.noreply.github.com>
2020-11-29 19:56:06 -0500

    Update starter.py

--- starter.py
+++ starter.py
@@ -7,8 +7,8 @@ def add(num1, num2):
 # CODE
 
 operation = input("Enter an operation\n=> add\n=>subtract\n=>multiply\n=>divide\n")
-first = input("Enter a number")
-second = input("Enter another number")
+first = int(input("Enter a number: "))
+second = int(input("Enter another number: "))
 
 if operation == "add":
   add(first, second)
